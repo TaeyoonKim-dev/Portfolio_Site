2402ce6a1c1bec288754a678b5f52590fef53e6c
@taeyoon_0526 <iamtaeyoon@icloud.com>
2024-08-01 14:31:30 +0900

    2024-08-01 - Supabase

--- functions/supabaseClient.mjs
+++ functions/supabaseClient.mjs
@@ -4,5 +4,5 @@ import { config } from 'dotenv';
 config(); // Load .env variables
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;  // 변경된 키 이름
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
