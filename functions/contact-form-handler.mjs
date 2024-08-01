e7129d6aff752742a094bc346f245e203d9b3615
@taeyoon_0526 <iamtaeyoon@icloud.com>
2024-08-01 14:35:50 +0900

    2024-08-01 - Supabase

--- functions/contact-form-handler.mjs
+++ functions/contact-form-handler.mjs
@@ -1,16 +1,14 @@
-import { supabase } from './supabaseClient.mjs';
+import {supabase} from "./supabaseClient.mjs";
 
 export const handler = async (event) => {
     try {
         const { name, email, message } = JSON.parse(event.body);
 
-        // 데이터베이스에 삽입
         const { data, error } = await supabase
             .from('contact_form')
             .insert([{ name, email, message }]);
 
         if (error) {
-            console.error('Error inserting data:', error);
             return {
                 statusCode: 500,
                 body: JSON.stringify({ error: 'Error inserting data into the database.' }),
@@ -22,7 +20,6 @@ export const handler = async (event) => {
             body: JSON.stringify({ message: 'Message received successfully!' }),
         };
     } catch (error) {
-        console.error('Error handling request:', error);
         return {
             statusCode: 500,
             body: JSON.stringify({ error: 'An error occurred while processing your request.' }),
